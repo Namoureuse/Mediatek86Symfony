e0b8dc1622cf07433ba66034f5bc8af247c68f94
Sonia <sonia.dakhli@etu.univ-lyon1.fr>
2022-04-04 02:52:55 +0200

    Mission 2 : Page d'ajout d'une formation.

--- src/Controller/admin/AdminFormationsController.php
+++ src/Controller/admin/AdminFormationsController.php
@@ -135,5 +135,27 @@ class AdminFormationsController extends AbstractController {
         $this->om->flush(); //permet d'envoyer des ordres vers la bdd
         return $this->redirectToRoute('admin.formations');
     }
+    
+    /**
+     * @Route("/admin/ajout", name="admin.formation.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response {
+        $formation = new Formation();
+        $formFormation = $this->createForm(FormationType::class, $formation);
+        
+        $formFormation->handleRequest($request);
+        if($formFormation->isSubmitted() && $formFormation->isValid()) {
+            $this->om->persist($formation); //enregistre en local la visite
+            $this->om->flush();
+            return $this->redirectToRoute('admin.formations');
+        }
+        
+        return $this->render("admin/admin.formation.ajout.html.twig", [
+            'formation' => $formation,
+            'formformation' => $formFormation->createView()
+        ]);
+    }
 
 }
